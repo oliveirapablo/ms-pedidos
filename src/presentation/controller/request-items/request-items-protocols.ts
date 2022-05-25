afed42850b8b5fb909bab6e122d34b9fe9491381
Pablo Santana <pablo.d.o.s96@gmail.com>
2022-05-25 12:56:27 -0300

    feat: add controller presentation to add a new requestItems

---
+++ src/presentation/controller/request-items/request-items-protocols.ts
@@ -0,0 +1,4 @@
+
+export * from '../../protocols'
+export * from '../../../domain/models/request-items'
+export * from '../../../domain/usecases/add-request-items'
---
+++ src/presentation/protocols/index.ts
@@ -0,0 +1,4 @@
+export * from './controller'
+export * from './http'
+export * from '../../domain/usecases/add-request-items'
+export * from '../../domain/models/request-items'
